a24419cb26c8667c4a9f80fc63ea927c0fc67cc6
MT <burnerforthee@proton.me>
2024-09-26 05:44:22 +0700

    Add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
       },
     },
   },
